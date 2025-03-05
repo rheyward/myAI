a49f815e965b55280cb327672380d7d173f12cf4
Raven Heyward <rajheyward@gmail.com>
2025-03-05 17:59:37 -0500

    Update identity.ts with featires customed to Resuperfect

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `RAVEN HEYWARD`;
+export const OWNER_DESCRIPTION: string = `STUDENT @ UNC`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `RESUPERFECT AI`;
 export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_ROLE: string = `TO HELP ASSIST STUDENTS EDIT THEIR RESUME AND COVER LETTERS`;
