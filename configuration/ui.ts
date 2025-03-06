75a5f408bbf8421b647f6aa70d2fd3d7dbb6adcc
Raven Heyward <rajheyward@gmail.com>
2025-03-06 11:18:21 -0500

    added empty citation message

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -6,3 +6,4 @@ export const FOOTER_MESSAGE: string = `CUSTOMIZED AI BY RAVEN HEYWARD @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const EMPTY_CITATION_MESSAGE: string = `hi`;
