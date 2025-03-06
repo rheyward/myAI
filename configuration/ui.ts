dccb3c0c37bd1bb444652095ed33e1130fa44f5d
Raven Heyward <rajheyward@gmail.com>
2025-03-06 11:12:18 -0500

    revert back to working

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,13 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
 export const FOOTER_MESSAGE: string = `CUSTOMIZED AI BY RAVEN HEYWARD @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
-
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI `;
-
-export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
-
-
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
