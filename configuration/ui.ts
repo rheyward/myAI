733e1d0a0f0e16339c71fa0d20b3f8c6a6988460
Raven Heyward <rajheyward@gmail.com>
2025-03-06 11:31:11 -0500

    customizations

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `RESUPERFECT`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `CUSTOMIZED AI BY RAVEN HEYWARD @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
+export const PAGE_TITLE: string = `RAVEN'S CHATBOT`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 export const EMPTY_CITATION_MESSAGE: string = `hi`;
